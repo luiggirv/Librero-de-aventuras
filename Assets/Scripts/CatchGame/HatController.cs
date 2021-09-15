37d2ae64ee4dccd620bb31319226841f9bd10ffd
Luiggi <a20152396@pucp.pe>
2021-09-14 21:34:02 -0500

    Modificaciones en Gameplay Catch Game

--- Assets/Scripts/CatchGame/HatController.cs
+++ Assets/Scripts/CatchGame/HatController.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HatController : MonoBehaviour {
+public class HatController : MonoBehaviour
+{
 
 	public Camera cam;
 	private float maxWidth;
@@ -18,27 +19,15 @@ public class HatController : MonoBehaviour {
 		float hatWidth = GetComponent<Renderer>().bounds.extents.x;
 		maxWidth = targetWidth.x-hatWidth;
 	}
-	
+
 	void FixedUpdate () {
 		if (canControl) {
-			//Vector3 rawPosition = cam.ScreenToWorldPoint (Input.mousePosition);
-			//Vector3 targetPosition = new Vector3 (rawPosition.x, 0.0f, 0.0f);
-			//float targetWidth = Mathf.Clamp (targetPosition.x, -maxWidth, maxWidth);
-			//targetPosition = new Vector3 (targetWidth, targetPosition.y, targetPosition.z);
-			//Vector3 targetPosition = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y,				 0.0f);
-			if (Input.touchCount > 0)
-            {
-				Touch touch = Input.GetTouch(0);
-				Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-				touchPosition.z = 0;
-				Vector3 direction = (touchPosition - transform.position);
-				GetComponent<Rigidbody2D>().velocity = new Vector2(direction.x, direction.y) * (10f);
-				if (touch.phase == TouchPhase.Ended)
-                {
-					GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-				}
-			}
-			//GetComponent<Rigidbody2D>().MovePosition(targetPosition);
+			Vector3 rawPosition = cam.ScreenToWorldPoint (Input.mousePosition);
+			Vector3 targetPosition = new Vector3 (rawPosition.x, 0.0f, 0.0f);
+			float targetWidth = Mathf.Clamp (targetPosition.x, -maxWidth, maxWidth);
+			targetPosition = new Vector3 (targetWidth, targetPosition.y, targetPosition.z);
+			GetComponent<Rigidbody2D>().MovePosition(targetPosition);
+
 		}
 	}
 
--- Assets/Scripts/CatchGame/Score.cs
+++ Assets/Scripts/CatchGame/Score.cs
@@ -15,17 +15,25 @@ public class Score : MonoBehaviour {
 		UpdateScore ();
 	}
 
-	void OnTriggerEnter2D () {
-		score += ballValue;
-		UpdateScore ();
-	}
-
-	void OnCollisionEnter2D (Collision2D collision) {
-		if (collision.gameObject.tag == "Bomb" || collision.gameObject.tag == "Bottle" || collision.gameObject.tag == "GlassBottle" || collision.gameObject.tag == "TrashCan") {
+	void OnTriggerEnter2D (Collider2D collision) {
+		if (collision.gameObject.tag == "Bomb")
+		{
 			score -= ballValue * 2;
-			UpdateScore ();
+			UpdateScore();
+		}
+		else
+		{
+			score += ballValue;
+			UpdateScore();
 		}
 	}
+
+	//void OnCollisionEnter2D (Collision2D collision) {
+	//	if (collision.gameObject.tag == "Bomb" || collision.gameObject.tag == "Bottle" || collision.gameObject.tag == "GlassBottle" || collision.gameObject.tag == "TrashCan") {
+	//		score -= ballValue * 2;
+	//		UpdateScore ();
+	//	}
+	//}
 
 	void UpdateScore () {
 		scoreText.text = "Puntaje\n" + score;
