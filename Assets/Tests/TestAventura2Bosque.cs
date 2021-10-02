164c9e818a4e648735d85c899de87d8bce614ba3
Luiggi <a20152396@pucp.pe>
2021-10-01 19:34:34 -0500

    Pruebas unitarias Aventura 2.2 terminadas

--- Assets/Tests/TestAventura2Bosque.cs
+++ Assets/Tests/TestAventura2Bosque.cs
@@ -19,6 +19,50 @@ public class TestAventura2Bosque
     [UnityTest]
     public IEnumerator TestAventura2BosqueHealthSystem()
     {
+        //Se crea un objeto con la clase Artifact para que aparezca en el escenario
+        var corral = new GameObject();
+        corral.AddComponent<Artifact>();
+        var audioSource = new GameObject();
+        audioSource.AddComponent<AudioSource>();
+        var sonidoCorral = new GameObject();
+        sonidoCorral.AddComponent<AudioSource>();
+        var player = new GameObject();
+        player.AddComponent<PlayerBackpack>();
+
+        //Se inicializan los parametros de la clase Artifact
+        corral.GetComponent<Artifact>().health = 100;
+        corral.GetComponent<Artifact>().maxHealth = 100;
+        corral.GetComponent<Artifact>().bleed = 0;
+        corral.GetComponent<Artifact>().audioSource = audioSource;
+        corral.GetComponent<Artifact>().sonidoCorral = sonidoCorral;
+        player.GetComponent<PlayerBackpack>().max = 50; //Max capacidad de frutas en la mochila
+        player.GetComponent<PlayerBackpack>().current = 40; //Número de frutas actualmente
+        corral.GetComponent<Artifact>().player = player;
+        corral.gameObject.tag = "Artifact";
+
+        //Se crea un GameObject que contenga la clase Manager
+        var manager = new GameObject();
+        manager.AddComponent<Manager>();
+        var gameOverUI = new GameObject();
+        var puntajeFinalText = new GameObject();
+        puntajeFinalText.AddComponent<Text>();
+
+        //Se inicializan los parametros de la clase Manager con tiempo inicial 60 segundos
+        manager.GetComponent<Manager>().timeToWin = 60.0f;
+        manager.GetComponent<Manager>().gameOverUI = gameOverUI;
+        manager.GetComponent<Manager>().PuntajeFinalText = puntajeFinalText;
+        manager.GetComponent<Manager>().artifact = corral.GetComponent<Artifact>();
+
+        //Inicializamos al objeto animal "Wolf" para que ataque el corral
+        var animal = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Importaciones/TowerDefense/Prefabs/wolf3.prefab");
+        animal = GameObject.Instantiate(animal, new Vector3(0, 0, 10), new Quaternion(0, 180, 0, 0));
+
+        //Guardamos el valor inicial de la vida del corral
+        var oldHealth = corral.GetComponent<Artifact>().health;
+        //Esperamos 5 segundos para que encuentre el corral y empiece a atacar
+        yield return new WaitForSeconds(5);
+        //Comprobamos que la vida del corral ha bajado por los ataques del animal
+        Assert.IsTrue(oldHealth > corral.GetComponent<Artifact>().health);
 
         // Use yield to skip a frame.
         yield return null;
@@ -27,13 +71,13 @@ public class TestAventura2Bosque
     [UnityTest]
     public IEnumerator TestAventura2BosqueTimeGlobal()
     {
-        //Se crea el componente Artifact para que aparezca en el escenario
+        //Se crea un objeto con clase Artifact para que aparezca en el escenario
         var corral = new Artifact();
         var audioSource = new GameObject();
         var sonidoCorral = new GameObject();
         var player = new GameObject();
 
-        //Se inicializan los parametros del componente Artifact
+        //Se inicializan los parametros de la clase Artifact
         corral.health = 120;
         corral.maxHealth = 120;
         corral.bleed = 0;
@@ -41,26 +85,25 @@ public class TestAventura2Bosque
         corral.sonidoCorral = sonidoCorral;
         corral.player = player;
 
-        //Se crea un GameObject que contenga el compontente Manager
+        //Se crea un GameObject que contenga la clase Manager
         var manager = new GameObject();
         manager.AddComponent<Manager>();
         var gameOverUI = new GameObject();
         var puntajeFinalText = new GameObject();
         puntajeFinalText.AddComponent<Text>();
 
-        //Se inicializan los parametros del componente Manager con tiempo inicial 60 segundos
+        //Se inicializan los parametros de la clase Manager con tiempo inicial 60 segundos
         manager.GetComponent<Manager>().timeToWin = 60.0f;
         manager.GetComponent<Manager>().gameOverUI = gameOverUI;
         manager.GetComponent<Manager>().PuntajeFinalText = puntajeFinalText;
         manager.GetComponent<Manager>().artifact = corral;
 
-        Debug.Log(manager.GetComponent<Manager>().GetTime());
-        //Se crea el componente PauseMenuEcosistema para que aparezca en el escenario
+        //Se crea la clase PauseMenuEcosistema para que aparezca en el escenario
         var pauseMenu = new GameObject();
         pauseMenu.AddComponent<PauseMenuEcosistema>();
         var pauseMenuUI = new GameObject();
 
-        //Se inicializan los parametros del componente PauseMenuEcosistema
+        //Se inicializan los parametros de la clase PauseMenuEcosistema
         pauseMenu.GetComponent<PauseMenuEcosistema>().pauseMenuUI = pauseMenuUI;
 
         //Pausar el tiempo
@@ -70,7 +113,6 @@ public class TestAventura2Bosque
 
         //Reanudar el tiempo
         pauseMenu.GetComponent<PauseMenuEcosistema>().Resume();
-
         var newTime = manager.GetComponent<Manager>().GetTime(); //Se captura el tiempo actual
         yield return new WaitForSeconds(5); //Se espera 5 segundos para evaluar si el tiempo cambia cuando está pausado
         Assert.AreNotEqual(newTime, manager.GetComponent<Manager>().GetTime()); // Se evalua si los valores no son iguales
@@ -82,7 +124,7 @@ public class TestAventura2Bosque
     [UnityTest]
     public IEnumerator TestAventura2BosqueScoreGlobal()
     {
-        //Se crea el componente Artifact para que aparezca en el escenario
+        //Se crea un objeto que contenga la clase Artifact para que aparezca en el escenario
         var corral = new GameObject();
         corral.AddComponent<Artifact>();
         var audioSource = new GameObject();
@@ -92,9 +134,9 @@ public class TestAventura2Bosque
         var player = new GameObject();
         player.AddComponent<PlayerBackpack>();
 
-        //Se inicializan los parametros del componente Artifact
-        corral.GetComponent<Artifact>().health = 120;
-        corral.GetComponent<Artifact>().maxHealth = 120;
+        //Se inicializan los parametros de la clase Artifact
+        corral.GetComponent<Artifact>().health = 100;
+        corral.GetComponent<Artifact>().maxHealth = 100;
         corral.GetComponent<Artifact>().bleed = 0;
         corral.GetComponent<Artifact>().audioSource = audioSource;
         corral.GetComponent<Artifact>().sonidoCorral = sonidoCorral;
@@ -103,26 +145,26 @@ public class TestAventura2Bosque
         corral.GetComponent<Artifact>().player = player;
         corral.gameObject.tag = "Artifact";
 
-        //Se crea un GameObject que contenga el compontente Manager
+        //Se crea un GameObject que contenga la clase Manager
         var manager = new GameObject();
         manager.AddComponent<Manager>();
         var gameOverUI = new GameObject();
         var puntajeFinalText = new GameObject();
         puntajeFinalText.AddComponent<Text>();
 
-        //Se inicializan los parametros del componente Manager con tiempo inicial 60 segundos
+        //Se inicializan los parametros de la clase Manager con tiempo inicial 60 segundos
         manager.GetComponent<Manager>().timeToWin = 60.0f;
         manager.GetComponent<Manager>().gameOverUI = gameOverUI;
         manager.GetComponent<Manager>().PuntajeFinalText = puntajeFinalText;
         manager.GetComponent<Manager>().artifact = corral.GetComponent<Artifact>();
 
-        //Se crea un GameObject que contenga el compontente ScoreGlobalTower
+        //Se crea un GameObject que contenga la clase ScoreGlobalTower
         var scoreGlobal = new GameObject();
         scoreGlobal.AddComponent<ScoreGlobalTower>();
         var puntajeText = new GameObject();
         puntajeText.AddComponent<Text>();
 
-        //Se inicializan los parametros del componente ScoreGlobalTower
+        //Se inicializan los parametros de la clase ScoreGlobalTower
         scoreGlobal.GetComponent<ScoreGlobalTower>().PuntajeReal = puntajeText;
         scoreGlobal.GetComponent<ScoreGlobalTower>().InternalScore = 0;
 
@@ -137,6 +179,7 @@ public class TestAventura2Bosque
         Assert.IsTrue(scoreGlobal.GetComponent<ScoreGlobalTower>().InternalScore*100 == (500 + 4000 + 100));
         //El puntaje total debería ser 4600 puntos.
 
+        //Se inicia el prefab Wolf para que busque el corral y ataque
         var animal = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Importaciones/TowerDefense/Prefabs/wolf3.prefab");
         animal = GameObject.Instantiate(animal, new Vector3(0, 0, 10), new Quaternion(0, 180, 0, 0));
 
